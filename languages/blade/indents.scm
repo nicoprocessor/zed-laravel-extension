e1fec33cc9cc319264b798512b53ed6ff42e4eff
Charalampos Raftopoulos <harrisrafto@gmail.com>
2026-02-23 21:47:26 +0200

    fix: blade language config for Zed compatibility

--- languages/blade/indents.scm
+++ languages/blade/indents.scm
@@ -1,6 +1,2 @@
-(directive_start) @indent.begin
-(directive_end) @indent.end
-
-(start_tag ">" @end) @indent
-(self_closing_tag "/>" @end) @indent
 (element (start_tag) @start (end_tag)? @end) @indent
+(self_closing_tag) @indent
